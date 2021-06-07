38208639de378208520e510324bb952206ad7dce
Michał Litwin <michaleklitwin@gmail.com>
2021-06-07 14:44:02 +0200

    add the 'main thing' and examples, added styles to the demo page

--- ch4.js
+++ ch4.js
@@ -7,10 +7,24 @@
 /** class representing a single element created by $4 */
 class CHElement {
     /**
+     * get elements by query string
+     * @param {String} selector the selector
+     * @param {Document | CHElement} parent the parent object to call `querySelectorAll()` on
+     * @returns {CHElement | Array<CHElement>} the element or an array of matches
+     */
+    static fromSelector(selector, parent) {
+        const query = parent.querySelectorAll(selector);
+        if (query.length === 1) return new CHElement(query[0]);
+        return Array.from(query, e => new CHElement(e));
+    }
+
+    /**
      * create an element class from an element
-     * @param {Element | Node} e the element
+     * @param {Element | Node | Window} e the element
      */
     constructor(e) {
+        if (!(e instanceof Element || e instanceof Node || e instanceof Window))
+            throw new TypeError("`" + e + "`: object not an Element, Node or Window")
         this.e = e;
         this._eventListeners = {};
     }
@@ -48,5 +62,57 @@ class CHElement {
      */
     getElement() {
         return this.e;
+    }
+
+    /**
+     * querySelector() inside an element
+     * @param {String} selector the selector
+     * @returns {CHElement | Array<CHElement>} an element or an array of elements
+     */
+    find(selector) {
+        return CHElement.fromSelector(selector, this.e);
+    }
+
+    /**
+     * the element's `innerText`
+     * @returns {String} the text
+     */
+    get text() { return this.e.innerText; }
+
+    /**
+     * the element's `innerText`
+     * @param {String} text the text
+     */
+    set text(text) { this.e.innerText = text; }
+
+    /**
+     * the element's `innerHTML`
+     * @returns {String} the HTML string
+     */
+     get html() { return this.e.innerHTML; }
+
+     /**
+      * the element's `innerHTML`
+      * @param {String} text the HTML string
+      */
+     set html(text) { this.e.innerHTML = text; }
+}
+
+/**
+ * main thing
+ * @param {Element | Node | String | Function} a an element, node, function or selector string
+ * @returns {CHElement | Array<CHElement> | Function} an element, an array of elements or a function
+ */
+function $4(a) {
+    switch (typeof a) {
+        case "string":
+            return CHElement.fromSelector(a, document);
+        
+        case "function":
+            return $4(window).on("load", a);
+
+        case "object":
+            if (a instanceof Element || a instanceof Node || a instanceof Window) return new CHElement(a);
+            else throw new TypeError("`" + e + "`: object not an instance of Element, Node or Window");
     }
 }
